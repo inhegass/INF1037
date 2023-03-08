53ae33bd8051e4180f1a64a3b8d413deb95f0594
inhegass <112832963+inhegass@users.noreply.github.com>
2023-03-08 11:34:52 -0300

    Update exercicio1.c

--- exercicio1.c
+++ exercicio1.c
@@ -3,7 +3,7 @@
 int main(void) {
 	int dias;
 	printf("Qual o numero de dia(s)?\n");
-	scanf_s("%d", &dias);
+	scanf("%d", &dias);
 	int semanas = dias / 7;
 	int d = dias - semanas * 7;
 	printf("Sao %d semana(s) e %d dia(s).\n", semanas, d);
