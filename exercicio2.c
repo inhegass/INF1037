3ef4b000ea3221a3381ce29a368d7c93f1727b37
inhegass <112832963+inhegass@users.noreply.github.com>
2023-03-08 11:35:15 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -4,9 +4,9 @@ int main(void) {
 	float n1;
 	float n2;
 	printf("Qual a primeira nota do aluno?\n");
-	scanf_s("%f", &n1);
+	scanf("%f", &n1);
 	printf("Qual a segunda nota do aluno?\n");
-	scanf_s("%f", &n2);
+	scanf("%f", &n2);
 	float media = (n1 * 3 + n2 * 7) / 10;
 	printf("Nota 1: %.1f\n", n1);
 	printf("Nota 2: %.1f\n", n2);
