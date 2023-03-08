cba70aea12edc9f419ab2f3d8b30a34cda5c40d1
inhegass <112832963+inhegass@users.noreply.github.com>
2023-03-08 10:31:06 -0300

    Add files via upload

---
+++ exercicio1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int dias;
+	printf("Qual o numero de dia(s)?\n");
+	scanf_s("%d", &dias);
+	int semanas = dias / 7;
+	int d = dias - semanas * 7;
+	printf("Sao %d semana(s) e %d dia(s)", semanas, d);
+	return 0;
+}
---
+++ exercicio2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	float n1;
+	float n2;
+	printf("Qual a primeira nota do aluno?\n");
+	scanf_s("%f", &n1);
+	printf("Qual a segunda nota do aluno?\n");
+	scanf_s("%f", &n2);
+	float media = (n1 * 3 + n2 * 7) / 10;
+	printf("Nota 1: %.1f\n", n1);
+	printf("Nota 2: %.1f\n", n2);
+	printf("Media: %.1f\n", media);
+	return 0;
+}
